7d9ddcd4c852989250a5c8c3d5a814d0c60c2baf
Matt Diebolt <mdiebolt@gmail.com>
2015-02-12 22:48:49 -0600

    add nginx image

--- cmds/build.bash
+++ cmds/build.bash
@@ -7,5 +7,6 @@ build() {
   docker build -t middleman .
   docker run --name "$CONTAINER_NAME" middleman
   docker cp "$CONTAINER_NAME":/usr/src/app/build.tar.gz .
+  docker build -f Dockerfile.site -t site-container .
   docker rm "$CONTAINER_NAME"
 }
