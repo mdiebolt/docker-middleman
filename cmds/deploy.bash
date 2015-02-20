e95683b59c513439594859067a7adfac460b500d
Matt Diebolt <mdiebolt@gmail.com>
2015-02-19 22:35:14 -0600

    simplify deploy command

--- cmds/deploy.bash
+++ cmds/deploy.bash
@@ -4,9 +4,9 @@ init() {
 
 deploy() {
   for ip in $(digitalocean-ips); do
-    scp "$BUILT_SITE.tar.gz" "root@$ip:$BUILT_SITE.tar.gz"
-    ssh root@$ip "docker load -i $BUILT_SITE.tar.gz"
-    ssh root@$ip "docker rm -f $BUILT_SITE || true"
-    ssh root@$ip "docker run -d --name $BUILT_SITE -p 80:8000 $BUILT_SITE"
+    cat "$BUILT_SITE.tar.gz" | ssh root@$ip "docker load \
+      && ( docker rm -f $BUILT_SITE || true ) \
+      && docker run -d --name $BUILT_SITE -p 80:8000 $BUILT_SITE" &
   done
+  wait
 }
