92c73e54a01993f3c387c7c16de3e1311b9f570b
Matt Diebolt <mdiebolt@gmail.com>
2015-02-19 23:07:40 -0600

    add ssh options

--- cmds/deploy.bash
+++ cmds/deploy.bash
@@ -1,10 +1,12 @@
+SSH_OPTS="${SSH_OPTS:--o PasswordAuthentication=no -o StrictHostKeyChecking=no}"
+
 init() {
   cmd-export deploy
 }
 
 deploy() {
   for ip in $(digitalocean-ips); do
-    cat "$BUILT_SITE.tar.gz" | ssh root@$ip "docker load \
+    cat "$BUILT_SITE.tar.gz" | ssh -A $SSH_OPTS root@$ip "docker load \
       && ( docker rm -f $BUILT_SITE || true ) \
       && docker run -d --name $BUILT_SITE -p 80:8000 $BUILT_SITE" &
   done
